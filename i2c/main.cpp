016c2bfd15cdfacc474204158431dc0335a8fff4
FeralAI <13342258+FeralAI@users.noreply.github.com>
2024-03-06 17:31:42 -0600

    Cleanup I2C

--- i2c/main.cpp
+++ i2c/main.cpp
@@ -6,19 +6,24 @@
 #include "hardware/vreg.h"
 #include "hardware/clocks.h"
 
-#define GP2040CE_GUEST_ID 0x17
+#define NOW() to_us_since_boot(get_absolute_time())
+#define LOG(start) printf("%llu\n", NOW() - start);
+#define LOGD(size, diff) printf("%i, %llu\n", diff, size)
+#define LOGD_H() printf("size, micros\n");
 
-// static const uint I2C_BAUDRATE = 100000; // 100 kHz
-// static const uint I2C_BAUDRATE = 400000; // 400 kHz
-// static const uint I2C_BAUDRATE = 1000000; // 1 MHz
-static const uint I2C_BAUDRATE = 1500000; // 1.5 MHz
+#define I2C_SLAVE_ADDR 0x17
+
+// #define I2C_BAUDRATE 100000 // 100 kHz
+// #define I2C_BAUDRATE 400000 // 400 kHz
+#define I2C_BAUDRATE 1000000 // 1 MHz
+// #define I2C_BAUDRATE 3500000 // 3.5 MHz - max tested stable
 
 // For this example, we run both the master and slave from the same board.
 // You'll need to wire pin GP4 to GP6 (SDA), and pin GP5 to GP7 (SCL).
-static const uint I2C_SLAVE_SDA_PIN = PICO_DEFAULT_I2C_SDA_PIN; // 4
-static const uint I2C_SLAVE_SCL_PIN = PICO_DEFAULT_I2C_SCL_PIN; // 5
-static const uint I2C_MASTER_SDA_PIN = 6;
-static const uint I2C_MASTER_SCL_PIN = 7;
+#define I2C_SLAVE_SDA_PIN 4
+#define I2C_SLAVE_SCL_PIN 5
+#define I2C_MASTER_SDA_PIN 6
+#define I2C_MASTER_SCL_PIN 7
 
 #define GAMEPAD_JOYSTICK_MID 0x7FFF
 struct GamepadState
@@ -34,27 +39,32 @@ struct GamepadState
 	uint8_t rt {0};
 };
 
-static uint8_t buf[256] = {0};
-static uint64_t micros = 0;
+struct I2CData
+{
+	GamepadState state;
+	uint64_t timestamp = 0;
+};
 
 // Our handler is called from the I2C ISR, so it must complete quickly. Blocking calls /
 // printing to stdio may interfere with interrupt handling.
 static void i2c_slave_handler(i2c_inst_t *i2c, i2c_slave_event_t event) {
-	static int32_t receivedIndex = -1;
+	static uint8_t buf[256] = {0};
+	static int32_t receivedIndex = 0;
 
 	switch (event) {
-		case I2C_SLAVE_RECEIVE: // master has written some data
-			receivedIndex++;
+		case I2C_SLAVE_RECEIVE:
 			buf[receivedIndex] = i2c_read_byte_raw(i2c);
+			receivedIndex++;
 			break;
-		case I2C_SLAVE_REQUEST: // master is requesting data load from memory
+		case I2C_SLAVE_REQUEST:
 			// TODO: Do something useful?!?
 			break;
-		case I2C_SLAVE_FINISH: // master has signalled Stop / Restart
+		case I2C_SLAVE_FINISH:
 			{
-				uint32_t diff = to_us_since_boot(get_absolute_time()) - micros;
-				printf("MICROS: %i, BYTES: %i, DATA: %X\n", diff, sizeof(GamepadState), *reinterpret_cast<GamepadState *>(buf));
-				receivedIndex = -1;
+				I2CData *data = reinterpret_cast<I2CData *>(buf);
+				uint64_t diff = NOW() - data->timestamp;
+				LOGD(diff, receivedIndex);
+				receivedIndex = 0;
 				memcpy(buf, 0, 256);
 			}
 			break;
@@ -63,7 +73,7 @@ static void i2c_slave_handler(i2c_inst_t *i2c, i2c_slave_event_t event) {
 	}
 }
 
-static void setup_slave() {
+void setup_slave() {
 	gpio_init(I2C_SLAVE_SDA_PIN);
 	gpio_set_function(I2C_SLAVE_SDA_PIN, GPIO_FUNC_I2C);
 	gpio_pull_up(I2C_SLAVE_SDA_PIN);
@@ -73,14 +83,12 @@ static void setup_slave() {
 	gpio_pull_up(I2C_SLAVE_SCL_PIN);
 
 	i2c_init(i2c0, I2C_BAUDRATE);
-	// configure I2C0 for slave mode
-	i2c_slave_init(i2c0, GP2040CE_GUEST_ID, &i2c_slave_handler);
+	i2c_slave_init(i2c0, I2C_SLAVE_ADDR, &i2c_slave_handler);
 }
 
-static void run_master() {
+void setup_master() {
 	gpio_init(I2C_MASTER_SDA_PIN);
 	gpio_set_function(I2C_MASTER_SDA_PIN, GPIO_FUNC_I2C);
-	// pull-ups are already active on slave side, this is just a fail-safe in case the wiring is faulty
 	gpio_pull_up(I2C_MASTER_SDA_PIN);
 
 	gpio_init(I2C_MASTER_SCL_PIN);
@@ -88,28 +96,16 @@ static void run_master() {
 	gpio_pull_up(I2C_MASTER_SCL_PIN);
 
 	i2c_init(i2c1, I2C_BAUDRATE);
+}
 
-	uint16_t i = 0;
-	while (1) {
-		GamepadState *state = new GamepadState {
-			.dpad = 0,
-			.buttons = i,
-			.aux = 0,
-			.lx = 0,
-			.ly = 0,
-			.rx = 0,
-			.ry = 0,
-			.lt = 0,
-			.rt = 0,
-		};
+void loop_master() {
+	LOGD_H();
+	I2CData data;
 
-		// printf("%i %X\n", i, *state);
-
-		micros = to_us_since_boot(get_absolute_time());
-		i2c_write_blocking(i2c1, GP2040CE_GUEST_ID, (const uint8_t *)state, sizeof(GamepadState), false);
-		i++;
-
-		delete state;
+	for (uint16_t i = 0; ; ++i) {
+		data.state.buttons = i;
+		data.timestamp = NOW();
+		i2c_write_blocking(i2c1, I2C_SLAVE_ADDR, (uint8_t *)&data, sizeof(I2CData), false);
 
 		sleep_ms(1000);
 	}
@@ -117,7 +113,16 @@ static void run_master() {
 
 int main() {
 	stdio_init_all();
+	sleep_ms(3000);
+
 	setup_slave();
-	run_master();
-	while (1);
+	setup_master();
+
+	sleep_ms(2000);
+	printf("I2C Master/Slave example\n");
+	printf("I2C rate set to %u KHz\n", I2C_BAUDRATE / 1000);
+	printf("Starting I2C transfer\n");
+	sleep_ms(1000);
+
+	loop_master();
 }
